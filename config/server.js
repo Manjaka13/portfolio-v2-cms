5b0b804865c2fb6ddf4ea3c432b41042fe763fa8
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-09-28 15:24:44 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5bb9dff79ef0946508ca5905a6519123'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b5b7e713-0705-489f-89d3-14bfcd44da50'
+};
